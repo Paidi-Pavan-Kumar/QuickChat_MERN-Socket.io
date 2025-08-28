70d5d13b1dfdc8171624897e806aabda0654954d
Paidi-Pavan-Kumar <paidipavanchowdary@gmail.com>
2025-08-28 13:51:03 +0530

    Added Socket server

--- server/server.js
+++ server/server.js
@@ -3,15 +3,45 @@ import 'dotenv/config'
 import cors from 'cors'
 import http from 'http'
 import { connectDB } from './lib/db.js'
+import userRouter from './routes/userRoutes.js'
+import messageRouter from './routes/messageRoutes.js'
+import {Server} from "socket.io"
 
 const app = express()
 const server = http.createServer(app)
 
+//intialize socket.io server
+export const io = new Server(server, {
+    cors : {origin : "*"}
+})
+
+//store online users
+export const userSocketMap = {}; // stored like{ userId : socketId}
+
+io.on("connection", (socket) => {
+    const userId = socket.handshake.query.userId;
+    console.log("User Connected", userId);
+
+    if (userId) userSocketMap[userId] = socket.id;
+
+    // Emit online users to all connected clients
+    io.emit("getOnlineUsers", Object.keys(userSocketMap));
+
+    socket.on("disconnect", () => {
+        console.log("User Disconnected", userId);
+        delete userSocketMap[userId];
+        io.emit("getOnlineUsers", Object.keys(userSocketMap));
+    })
+})
+
+
 app.use(express.json({limit : "4mb"}))
 app.use(cors())
+await connectDB()
 
 app.use("/api/status", (req, res) => res.send("server is live"))
-await connectDB()
+app.use("/api/auth", userRouter)
+app.use("/api/messages", messageRouter)
 const PORT = process.env.PORT || 5000
 
 server.listen(PORT, () => console.log("server is running on PORT :" + PORT))
